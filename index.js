c20a9522389d4a6d8bb9bce9fba55434196f1dbe
L-Caro <lionelcaro74@gmail.com>
2023-02-24 15:33:17 +0100

    ajout express.secure

--- index.js
+++ index.js
@@ -2,18 +2,25 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
+const session = require('express-session');
 
 const routerPublic = require('./app/routers/routerPublic.js');
 const error404 = require('./app/middleware/404.js');
-
 
 const PORT = process.env.PORT || 3000;
 
 app.set('views', './app/views');
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false } // car en http et pas https
+  }));
+
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 //------------------------------------
